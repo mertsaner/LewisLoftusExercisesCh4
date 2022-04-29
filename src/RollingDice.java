dd8ba641cd43287f35db57959c56b283404726f7
mertsaner <mertsaner@windowslive.com>
2022-04-29 16:45:24 +0200

    First Commit

---
+++ src/RollingDice.java
@@ -0,0 +1,18 @@
+//******************************
+// RollingDice.java         Author: Lewis/Loftus
+//
+//  Demonstrates the creation and use of a user-defined class.
+//******************************
+public class RollingDice {
+    //--------------------------------
+    // Creates two Die objects and rolls them several times
+    //--------------------------------
+    public static void main(String[] args)
+    {
+        Die die1, die2;
+        int sum;
+
+        die1 = new Die (); // Creating new die object (named die1) from Die Class
+        die2 = new Die (); // Creating new die object (named die2) from Die Class
+    }
+}
