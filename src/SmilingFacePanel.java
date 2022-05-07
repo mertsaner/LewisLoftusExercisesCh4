610fb0b4bb76541ee55e231f40d7c4e97d46ede1
mertsaner <mertsaner@windowslive.com>
2022-05-07 09:43:57 +0200

    SmilingFace.java

--- src/SmilingFacePanel.java
+++ src/SmilingFacePanel.java
@@ -5,10 +5,49 @@
 //********************************************************************
 
 import javax.swing.*;
-import javax.swing.Jpanel;
 import java.awt.*;
 
 public class SmilingFacePanel extends JPanel {
 
-    private final int BASEX = 120, BASEY = 60;
+    private final int BASEX = 120, BASEY = 60;      // base point for head
+    //--------------------------------------------------------------------------------
+    //  Constructor: Sets up the main characteristics of this panel.
+    //--------------------------------------------------------------------------------
+    public SmilingFacePanel()
+    {
+        setBackground(Color.blue);
+        setPreferredSize(new Dimension(320,200));
+        setFont(new Font ("Arial", Font.BOLD, 16));
+    }
+
+    //--------------------------------------------------------------------------------
+    //  Draws a face.
+    //--------------------------------------------------------------------------------
+    public void paintComponent (Graphics page){
+
+        super.paintComponent(page);
+
+        page.setColor(Color.yellow);        //color of the shape set
+        //Drawing the shape start
+        page.fillOval(BASEX, BASEY,80,80); // head
+        page.fillOval(BASEX-5, BASEY+20, 90, 40);   // ears
+
+        page.setColor(Color.black);
+        page.drawOval(BASEX+20, BASEY+30, 15, 7);   // eyes
+        page.drawOval(BASEX+45, BASEY+30, 15,7);
+
+        page.fillOval(BASEX+25, BASEY+31, 5,5); // pupils
+        page.fillOval(BASEX+50, BASEY+31, 5,5);
+
+        page.drawArc(BASEX+20, BASEY+25, 15, 7, 0, 180);    // eyebrows
+        page.drawArc(BASEX+45, BASEY+25, 15, 7, 0, 180);
+
+        page.drawArc(BASEX+35, BASEY+40, 15, 10, 180, 180); //nose
+        page.drawArc(BASEX+20, BASEY+50, 40, 15, 180, 180); //mouth
+
+        page.setColor(Color.white);
+        page.drawString("Always remember that you are unique!", BASEX-105, BASEY-15);
+        page.drawString("Just like everyone else. ", BASEX-65, BASEY+105);
+        page.drawString("\"Allah herkesi ozel yaratti\" ", BASEX-65, BASEY+130);
+    }
 }
