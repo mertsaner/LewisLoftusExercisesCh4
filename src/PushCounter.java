e935d9cc991ee837d6b1de11e671cab782e9f016
mertsaner <mertsaner@windowslive.com>
2022-05-12 15:41:21 +0200

    Update

--- src/PushCounter.java
+++ src/PushCounter.java
@@ -1,5 +1,23 @@
 //*******************************************************************************
+//  PushCounter.java            Author: Lewis/Loftus
+//
+//  Demonstrates a graphical user interface and an event listener.
+//*******************************************************************************
+import javax.swing.JFrame;
 
 public class PushCounter {
 
+    //----------------------------------------------------------------------------
+    //  Creates the main program frame.
+    //----------------------------------------------------------------------------
+
+    public static void main (String[] args){
+
+        JFrame frame = new JFrame("Push Counter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(new PushCounterPanel());
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
---
+++ src/PushCounterPanel.java
@@ -0,0 +1,50 @@
+//*************************************************************************
+//      PushCounterPanel.java           Author: Lewis/Loftus
+//
+//      Demonstrates a graphical user interface and an event listener.
+//*************************************************************************
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class PushCounterPanel extends JPanel {
+
+    private int count;
+    private JButton push;
+    private JLabel label;
+
+    //-------------------------------------------------------------
+    //  Constructor: Sets up the GUI.
+    //-------------------------------------------------------------
+
+    public PushCounterPanel() {
+
+        count = 0;
+
+        push = new JButton("Push me!");
+        push.addActionListener(new ButtonListener());
+
+        label = new JLabel("Pushes:" + count);
+
+        add(push);
+        add(label);
+
+        setPreferredSize(new Dimension(300, 400));
+        setBackground(Color.cyan);
+    }
+
+    //****************************************************************************
+    // Represents a listener for button push(action) events.
+    //****************************************************************************
+    private class ButtonListener implements ActionListener
+    {
+        //------------------------------------------------------------------------------
+        //  Updates the counter and label when the button is pushed.
+        //-------------------------------------------------------------------------------
+        public void actionPerformed(ActionEvent event){
+            count++;
+            label.setText("Pushes: " + count);
+        }
+    }
+}
