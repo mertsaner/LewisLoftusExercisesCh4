1f919b5acb044ccfdf0faa5146a2cb880cab945f
mertsaner <mertsaner@windowslive.com>
2022-05-16 19:34:59 +0200

    Update

---
+++ src/test2.java
@@ -0,0 +1,9 @@
+public class test2 {
+
+    public static void main(String[] args) {
+        //String Concatenation in JAVA
+        String test = " ";
+        test = " first" + " second" + " third" + "rand";
+        System.out.println(test);
+    }
+}
