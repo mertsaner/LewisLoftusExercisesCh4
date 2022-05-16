864b254497c8e6794d5259d36e2aacf052bcd800
mertsaner <mertsaner@windowslive.com>
2022-05-16 16:46:48 +0200

    Small Update

--- src/PushCounter.java
+++ src/PushCounter.java
@@ -13,11 +13,11 @@ public class PushCounter {
 
     public static void main (String[] args){
 
-        JFrame frame = new JFrame("Push Counter");
+        JFrame frame = new JFrame("My Counter"); // Updated Push Counter
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        frame.getContentPane().add(new PushCounterPanel());
+        frame.getContentPane().add(new PushCounterPanel()); // new PushCounterPanel object passed to the add method of java.awt.Component package
         frame.pack();
-        frame.setVisible(true);
+        frame.setVisible(true);     //Enables that JFrame will be visible to us
     }
 }
