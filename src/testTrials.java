383920d1f109ba5ceb43d5a8eee01aa9ac30c3aa
mertsaner <mertsaner@windowslive.com>
2022-05-16 18:29:04 +0200

    Update

--- src/testTrials.java
+++ src/testTrials.java
@@ -11,22 +11,20 @@ public class testTrials {
 
         System.out.println("Please enter an input");
         // Declaration and Initialization of Variables
-        int input = 0;
         int count = 0;
-        String s2 = "";
+        String myString = "0";
+        String earlier = "";
         boolean always = true;
         Scanner scan = new Scanner(System.in);
 
-        s2 = Integer.toString(count);    //string s2 is equal to new trial in each turn
-        System.out.println("Number of Trials equals to: " + s2);
-        String earlier = s2;
+        System.out.println("Number of Trials equals to: " + myString);
         count = 1; // add to proper start
         while (always) {           //Keep receiving input until user enter an input which is bigger than 0
-            input = scan.nextInt();     //scan object uses nextInt() method which defined in Scanner class
-            earlier = Integer.toString(count);
+            int input = scan.nextInt();     //scan object uses nextInt() method which defined in Scanner class
+            earlier = Integer.toString(count); //string earlier is equal to new trial in each turn
             count++;
-            s2 =  s2 + earlier ;
-            System.out.println("Number of Trials equals to: "+  s2 );
+            myString =  myString + earlier ;
+            System.out.println("Number of Trials equals to: "+  myString );
         }
     }
 }
