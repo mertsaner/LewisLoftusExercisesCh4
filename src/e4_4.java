0e49c58c2df14eedff5df931c17ceec8952a6460
mertsaner <mertsaner@windowslive.com>
2022-05-18 06:04:02 +0300

    Update

--- src/e4_4.java
+++ src/e4_4.java
@@ -2,6 +2,17 @@ public class e4_4 {
 
     public static void main(String[] args) {
 
-        System.out.println("Hi there!");
+
+
+    }
+
+    /*
+    *   Write a method called synonyms that prints
+    *   at least two synonyms of a word when invoked.
+    *   The method should accept no parameters and return no value.
+    */
+
+    public void synonyms (){
+        System.out.println("Hi there, I am synonyms");
     }
 }
