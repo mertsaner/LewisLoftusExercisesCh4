a94e2203b1350d2ea897e55acdce5c39aff6971d
mertsaner <mertsaner@windowslive.com>
2022-06-29 14:50:30 +0200

    p4_1

--- src/NumberOfGoals.java
+++ src/NumberOfGoals.java
@@ -1,6 +1,6 @@
-/**    Answer based on the Question : Write a class called NumberOfGoals that represents the total number of goals
-*      scored by a football team. The NumberOfGoals class should contain a
-*      single integer as instance data, represent- ing the number of goals
+/**Answer based on the Question: Write a class called NumberOfGoals that represents
+ * the total number of goals scored by a football team. The NumberOfGoals class should contain a
+*      single integer as instance data, representing the number of goals
         scored. Write a constructor to initialize the number of goals to zero.
         Write a method called setGoal that increments the value by one
         whenever a goal is scored,
@@ -12,13 +12,69 @@
 
 public class NumberOfGoals {
 
-    static int goalsScored = 0;
+    //------------------------------------------------------------------------------
+    // Creates two Goals object and make them goal several times
+    //------------------------------------------------------------------------------
 
-    public static int getGoal() {
-        return goalsScored;
+    public static void main(String[] args) {
+        Goals goal1, goal2;
+        int sum;                        //Number of Goals scored declared
+
+        goal1 = new Goals (); // Creating new Goals object (named object1) from Die Class
+        goal2 = new Goals (); // Creating new Goals object (named object1) from Die Class
+
+
+        //goal1.comboGoal();
+        //goal1.goalMaker();
+        goal2.setGoals(12);
+
+        System.out.println("Goal1: " + goal1);
+        System.out.println("Goal2: " + goal2);
+
+        sum = goal1.getGoals() + goal2.getGoals();
+        System.out.println("Sum of the Goals: " + sum);
+
+        System.out.println("Goalmaker function called!");
+        sum = goal1.goalMaker() + goal2.goalMaker();
+        System.out.println("Goal1: " + goal1);
+        System.out.println("Goal2: " + goal2);
+        System.out.println("New sum: " + sum);
+
     }
-    public void GoalTracker() {
-        NumberOfGoals firstObject = new NumberOfGoals();
-        NumberOfGoals secondObject = new NumberOfGoals();
+}
+
+class Goals {
+
+    private int currentGoal; // current value showing number of goals made
+
+    public Goals(){
+        currentGoal = 0;        //Starting value goal number in the match
+                                //Constructor of the class works as an initializer
+    }
+
+    public int comboGoal() {
+        currentGoal = currentGoal + 5;
+        return currentGoal;
+    }
+
+    public int goalMaker() {
+        currentGoal++;
+        return currentGoal;
+    }
+
+
+    public void setGoals(int value) {
+        currentGoal = value;
+    }
+
+    public int getGoals() {
+        return currentGoal;
+    }
+
+    public String toString()
+    {
+
+        String result = Integer.toString(currentGoal);
+        return result;
     }
 }
