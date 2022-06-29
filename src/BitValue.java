21f96685239d1b7e3c7f49acbbc6fb93a577080e
mertsaner <mertsaner@windowslive.com>
2022-06-29 15:09:05 +0200

    p4_2 Cretated

---
+++ src/BitValue.java
@@ -0,0 +1,15 @@
+public class BitValue {
+
+    public static void main (String[] args)
+    {
+
+
+    }
+}
+
+
+class Bits {            //Driver Class, in which we define getters,setters,toString and  certain functions related with logic of the program
+
+
+
+}
